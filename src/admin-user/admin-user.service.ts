f59736f738bc89f5eb653c8c88d132df2d91c5e5
danna leguizamon <dannaleguizamon@Air-de-danna.lan>
2025-07-21 22:55:03 -0400

    ajustes 21 jul v2

--- src/admin-user/admin-user.service.ts
+++ src/admin-user/admin-user.service.ts
@@ -110,7 +110,7 @@ export class AdminUserService {
     const user = await this.adminUserRepository
       .createQueryBuilder('user')
       .addSelect('user.password')
-      .leftJoinAndSelect('user.studio', 'studio') // Load the studio relation
+      .addSelect('user.studioId') // Explicitly select studioId
       .where('user.email = :email', { email })
       .getOne();
     return user || undefined;
